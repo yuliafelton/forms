d3b783c94665ba5ad4a506eab97d825a62058a66
yuliafelton <lorielika@mail.ru>
2021-10-21 18:57:42 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+    const [formData, setFormData] = useState({name: '', email:'', age:'', about:''});
+
+    useEffect(() =>{
+        console.log(formData);
+    }, [formData]);
+
+    const handleChange = e => {
+        setFormData({...formData, [e.target.id]: e.target.value});
+    };
+
+    const handleSubmit = (event) => {
+        console.log(formData);
+        event.preventDefault();
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" placeholder="Enter name" onChange={handleChange}/>
+
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" placeholder="Enter your email" onChange={handleChange} />
+
+          <label htmlFor="age">Age:</label>
+          <input type="number" id="age" placeholder="Enter your age" onChange={handleChange} />
+
+          <label htmlFor="about">About me:</label>
+          <textarea cols="50" rows="20" id="about" onChange={handleChange}>
+          </textarea>
+
+          <input type="submit" value="Submit"/>
+      </form>
     </div>
   );
 }

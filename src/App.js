5b35a3b54d3ab01f8e28a3ca1a8fc45703a9cb1f
yuliafelton <lorielika@mail.ru>
2021-10-22 16:05:12 +0300

    changed the app

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
 import React, {useEffect, useState} from 'react';
-import './App.css';
 
 function App() {
+
     const [formData, setFormData] = useState({name: '', email:'', age:'', about:''});
+    const forceUpdate = (changeObject) => setFormData({...formData, ...changeObject});
+    window.forceUpdate = forceUpdate;
 
     useEffect(() =>{
         console.log(formData);
     }, [formData]);
 
-    const handleChange = e => {
-        setFormData({...formData, [e.target.id]: e.target.value});
-    };
-
-    const handleSubmit = (event) => {
-        console.log(formData);
-        event.preventDefault();
-    };
-
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" placeholder="Enter name" onChange={handleChange}/>
-
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" placeholder="Enter your email" onChange={handleChange} />
-
-          <label htmlFor="age">Age:</label>
-          <input type="number" id="age" placeholder="Enter your age" onChange={handleChange} />
-
-          <label htmlFor="about">About me:</label>
-          <textarea cols="50" rows="20" id="about" onChange={handleChange}>
-          </textarea>
-
-          <input type="submit" value="Submit"/>
-      </form>
+      please see the console output for state changes
     </div>
   );
 }
